c213004f7e686cf6a6f327307fcc26de8cd47750
Krishna Agrawal <121168949+Kriagr23@users.noreply.github.com>
2024-02-07 20:04:11 +0530

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    if (window.location.hash) {
+        window.location.replace('/');
+    }
+
+    const loader = document.querySelector('.loader');
+    loader.style.display = 'none';
+    const main = document.querySelector('.main');
+    main.style.display = 'block';
+
+
+    /* For the drop-down to pop up on click of 'about' and 'event' button */
+    const about_btn = document.getElementById('about');
+    const event_btn = document.getElementById('event');
+
+    const aboutDropdown = document.querySelector('.about-dropdown');
+    const eventDropdown = document.querySelector('.event-dropdown');
+
+    about_btn.addEventListener('click', () => {
+        if(aboutDropdown.style.display === 'none' || aboutDropdown.style.display == ""){
+            aboutDropdown.style.display = 'block'
+        }else{
+            aboutDropdown.style.display = 'none'
+        }
+    });
+
+    aboutDropdown.addEventListener('click', () => {
+        aboutDropdown.style.display = 'none'
+        eventDropdown.style.display = 'none'
+    });
+
+    event_btn.addEventListener('click', () => {
+        if(eventDropdown.style.display === 'none' || eventDropdown.style.display == ""){
+            eventDropdown.style.display = 'block'
+        }else{
+            eventDropdown.style.display = 'none'
+        }
+    });
+
+    eventDropdown.addEventListener('click', () => {
+        eventDropdown.style.display = 'none'
+        aboutDropdown.style.display = 'none'
+    });
+
+
+    
+
+    /* For the side menu, that comes when clicked on the hamburger menu, which is visible when screen is < 800px */
+    const hamburger_btn = document.querySelector('.hamburger');
+    const closeMenu_btn = document.querySelector('.closeMenu');
+    const sideNavBar = document.querySelector('.side-navbar');
+
+    hamburger_btn.addEventListener('click', () => {
+        hamburger_btn.style.display = 'none';
+        closeMenu_btn.style.display = 'block';
+        sideNavBar.style.display = 'block';
+    });
+
+    closeMenu_btn.addEventListener('click', () => {
+        closeMenu_btn.style.display = 'none';
+        sideNavBar.style.display = 'none';
+        hamburger_btn.style.display = 'block';
+    });
+
+
+
+    
+    /* For the gallery scroll, with left and right buttons */
+    const scrollLeftButton = document.getElementById('scroll-left');
+    const scrollRightButton = document.getElementById('scroll-right');
+    const photoSlides = document.querySelector('.photos-slide');
+
+
+    let currentScroll = 0;
+
+    scrollLeftButton.addEventListener('click', () => {
+        const imageWidth = photoSlides.querySelector('img').clientWidth; //Get image width
+        currentScroll = Math.max(0, currentScroll - imageWidth); //Prevent scrolling beyond felt edge
+        photoSlides.scrollTo({ left: currentScroll, behavior: 'smooth' });
+    });
+
+    scrollRightButton.addEventListener('click', () => {
+        const imageWidth = photoSlides.querySelector('img').clientWidth; //Get image width
+        const maxScroll = photoSlides.scrollWidth - photoSlides.clientWidth;
+        currentScroll = Math.min(maxScroll, currentScroll + imageWidth); //Prevent scrolling beyond felt edge
+        photoSlides.scrollTo({ left: currentScroll, behavior: 'smooth' });
+    });
+});
